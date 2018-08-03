50ab95e9cf24be472efc1b2ff02106af4a9b6795
cooper-mj <coopermj@stanford.edu>
2018-08-02 17:52:13 -0700

    Rewrote to use iexfinance library instead of Alpha Vantage.

--- sma_graph.py
+++ sma_graph.py
@@ -1,184 +1,161 @@
 
-
-from alpha_vantage.timeseries import TimeSeries
-from alpha_vantage.techindicators import TechIndicators
-import datetime
-
+from iexfinance import get_historical_data
+from datetime import datetime
+from datetime import timedelta
 from termcolor import colored, cprint
-import sys
+
+# '''
+# Returns the N-day simple moving average for the given
+# ticker on the given date. Date must be passed in as a
+# datetime object. Price type must be in the set
+# { 'open', 'high', 'low', 'close' }.
+# '''
+# def N_day_sma(N, ticker, date, price_type='close'):
+# 	start_date = date - timedelta(days=N)
+# 	end_date = date
+# 	try:
+# 		df = get_historical_data(ticker, start=start_date, end=end_date, output_format='pandas')
+# 		return df[price_type].mean()
+# 	except ValueError:
+# 		print("The average starting at this date falls outside the 5-year limit on historical data that this API provides.")
+# 		return -1
+
+def N_day_sma_from_dataframe(N, dataframe, date, start_date, price_type):
+	# Check to make sure we can take an N day moving average with the data
+	# given - e.g. we can't take a 90-day average if we only have data
+	# for 5 days.
+	delta = date - start_date
+	if delta.days < N:
+		return -1
+
+	sma_start_date = date-timedelta(days=N)
+	formatted_sma_start_date = sma_start_date.strftime("%Y-%m-%d")
+	formatted_end_date = date.strftime("%Y-%m-%d")
+	rows = dataframe[(dataframe['date'] > formatted_sma_start_date) & (dataframe['date'] <= formatted_end_date)]
+
+	return rows[price_type].mean()
 
 
 '''
-Takes in an API key and a ticker symbol; returns a dictionary
-mapping dates to N-day simple moving averages for each date.
+E.g. if I was to 
 '''
-def N_day_sma(N, API_key, ticker, price_time):
-	#ti = TechIndicators(key=API_key)
-	N_day, N_md = ti.get_sma(ticker, 'daily', N, price_time)
+def simple_sma_trade(starting_capital, short_term_N, long_term_N, ticker, start_date, end_date, price_type='close'):
 
-	N_sma = {}
-	for day in N_day:
-		date = datetime.datetime.strptime(day, "%Y-%m-%d").date()
-		N_sma[date] = float(N_day[day]['SMA'])
-	return N_sma
+	print("SMA Trading Algorithm:")
+	print("Short term SMA: " + str(short_term_N) + " days.")
+	print("Long term SMA: " + str(long_term_N) + " days.")
 
-def price_data(API_key, ticker, price_time):
-	#ts = TimeSeries(key=API_key)
-	price, price_md = ts.get_daily(symbol=ticker, outputsize='full')
-	price_dict = {}
-	for day in price:
-		date = datetime.datetime.strptime(day, "%Y-%m-%d").date()
-		price_dict[date] = float(price[day][price_time])
-	return price_dict
+	# Credit to Hugo Rodger Brown (StackOverFlow) for this constructor notation: https://stackoverflow.com/questions/7274267/print-all-day-dates-between-two-dates
+	date_range = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
 
-def simple_sma_trade(API_key, starting_capital, ticker):
-	'''
-	Uses 30 and 90 day simple moving averages to compute buy and sell
-	signals. Buys and sells when the buy/sell signal changes. Buys and
-	sells full portfolio at each buy/sell signal.
-	'''
-
-	thirty_day_sma = N_day_sma(1, API_key, ticker, 'close')
-	ninety_day_sma = N_day_sma(2, API_key, ticker, 'close')
-	prices = price_data(API_key, ticker, '4. close') # the '4. close' instead of just 'close' is just one of the idiosynchronacies of this library.
-
-	# Ensure that all dates we will be evaluating are valid for 30 day SMA, 90 day SMA, and price tracking
-	cumulative_dates = sorted(list(set(thirty_day_sma.keys()) & set(ninety_day_sma.keys()) & set(prices)))
+	# Import stock data in the desired date range
+	df = get_historical_data(ticker, start=start_date, end=end_date, output_format='pandas')
+	df.index.name = 'date'
+	df.reset_index(inplace=True)
 
 	capital = starting_capital
 	num_shares = 0
-	status_buy = False
+	liquid = True
 	
 	previous_price = 0
 	correct = [0, 0]
 
-	print("Ready to trade!")
+	for date in date_range:
+		try:
+			price = float(df.loc[df["date"] == date.strftime("%Y-%m-%d")][price_type].iloc[0])
+			#at[date.strftime("%Y-%m-%d"), price_type]
+		except (KeyError, IndexError):
+			continue
 
-	for date in cumulative_dates:
+		short_term_sma = N_day_sma_from_dataframe(short_term_N, df, date, start_date, price_type)
+		long_term_sma = N_day_sma_from_dataframe(long_term_N, df, date, start_date, price_type)
 
-		if thirty_day_sma[date] > ninety_day_sma[date]:
-			# If thirty_day > ninety_day - sell signal
-			if status_buy:
-				status_buy = False
-				capital = num_shares * prices[date]
-				sell_msg = colored("Selling " + str(num_shares) + " at " + str(prices[date]) + " for " + str(capital), 'red')
-				print(date + " : " + sell_msg)
+		if short_term_sma == -1 or long_term_sma == -1:
+			# Then we can't get an N-day SMA - we are probably too early in the process
+			continue
+
+		if short_term_sma > long_term_sma:
+			# If short_term > long_term - sell signal
+			if not liquid:
+				liquid = True
+				capital = num_shares * price
+				sell_msg = colored("Selling " + str(num_shares) + " at " + str(price) + " for " + str(capital), 'red')
+				print(date.strftime("%Y-%m-%d") + " : " + sell_msg)
 				num_shares = 0
 
 				# Check to see if our last decision was the right one
-				if prices[date] > previous_price:
-					# Check if current price is more than the price we bought for since
-					# our last call was a buy
-					correct[0] += 1
-					correct[1] += 1
-				else:
-					correct[1] += 1
-
-				previous_price = prices[date]
-
-
-		else:
-			# If ninety_day > thirty_day - buy signal
-			if not status_buy:
-				status_buy = True
-				num_shares = float(capital) / prices[date]
-				buy_msg = colored("Buying " + str(num_shares) + " at " + str(prices[date]) + " for " + str(capital), 'green')
-				print(date + " : " + buy_msg)
-				capital = 0
-
-				# Check to see if our last decision was the right one
-				if prices[date] < previous_price:
+				if price > previous_price:
 					# Check if current price is less than price we bought for since
 					# our last call was a buy - check if we made money
 					correct[0] += 1
 					correct[1] += 1
 				else:
 					correct[1] += 1
-	
-				previous_price = prices[date]
 
+		else:
+			# If long_term > short_term - buy signal
+			if liquid:
+				liquid = False
+				num_shares = float(capital) / price
+				buy_msg = colored("Buying " + str(num_shares) + " at " + str(price) + " for " + str(capital), 'green')
+				print(date.strftime("%Y-%m-%d") + " : " + buy_msg)
+				capital = 0
+
+				# Check to see if our last decision was the right one
+				if price < previous_price:
+					# Check if current price is less than price we bought for since
+					# our last call was a buy - check if we made money
+					correct[0] += 1
+					correct[1] += 1
+				else:
+					correct[1] += 1
+
+		previous_price = price
 
 	if capital == 0:
 		# Sell the remaining shares usig the final date's price
-		capital = num_shares * prices[cumulative_dates[-1]]
+		capital = num_shares * previous_price
 		num_shares = 0
 
 	print("Finished with " + str(capital) + " dollars.")
 	print("Made money in " + str(correct[0]) + " out of " + str(correct[1]) + " decisions. Accuracy: " + str(float(correct[0])/correct[1]))
 
-	#buy_and_hold(API_key, starting_capital, ticker)
 
-def buy_and_hold(API_key, starting_capital, ticker):
+def buy_and_hold(starting_capital, ticker, start_date, end_date, price_type='close'):
+	
+	df = get_historical_data(ticker, start=start_date-timedelta(days=1), end=end_date+timedelta(days=1), output_format='pandas')
+	df.index.name = 'date'
+	df.reset_index(inplace=True)
 
-	prices = price_data(API_key, ticker, '4. close') # the '4. close' instead of just 'close' is just one of the idiosynchronacies of this library.
-	cumulative_dates = sorted(prices.keys())
+	# Get start and end dates, and prices associated with each
+	try:
+		start_price = float(df.loc[df["date"] == start_date.strftime("%Y-%m-%d")][price_type].iloc[0])
+	except (KeyError, IndexError):
+		new_start_date = min(date for date in df["date"] if date > start_date.strftime("%Y-%m-%d"))
+		print("Your selected start date was not found in the database. Starting on " + new_start_date + " instead of " + start_date.strftime("%Y-%m-%d") + ".")
+		start_price = float(df.loc[df["date"] == new_start_date][price_type].iloc[0])
+
+	try:
+		end_price = float(df.loc[df["date"] == end_date.strftime("%Y-%m-%d")][price_type].iloc[0])
+	except (KeyError, IndexError):
+		new_end_date = max(date for date in df["date"] if date < end_date.strftime("%Y-%m-%d"))
+		print("Your selected end date was not found in the database. Ending on " + new_end_date + " instead of " + end_date.strftime("%Y-%m-%d") + ".")
+		end_price = float(df.loc[df["date"] == new_end_date][price_type].iloc[0])
 
 	# Buy initially
 	capital = starting_capital
-	num_shares = float(capital) / prices[cumulative_dates[0]]
+	num_shares = float(capital) / start_price
 	capital = 0
 
 	# Sell finally
-	capital = num_shares * prices[cumulative_dates[-1]]
+	capital = num_shares * end_price
 	num_shares = 0
 
-	print("Finished with " + str(capital) + "dollars.")
-
-
-
-def plot_averages():
-	from alpha_vantage.timeseries import TimeSeries
-	from alpha_vantage.techindicators import TechIndicators
-	import datetime
-	import matplotlib.pyplot as plt
-
-	#ti = TechIndicators(key="Y9LU3HXNM65GKI9R")
-	thirty_day, thirty_md = ti.get_sma('.DJI', 'daily', 30, 'close')
-	ninety_day, ninety_md = ti.get_sma('.DJI', 'daily', 90, 'close')
-
-	thirty_dict = {}
-	for day in thirty_day:
-		date = datetime.datetime.strptime(day, "%Y-%m-%d").date()
-		thirty_dict[date] = float(thirty_day[day]['SMA'])
-
-	ninety_dict = {}
-	for day in ninety_day:
-		date = datetime.datetime.strptime(day, "%Y-%m-%d").date()
-		ninety_dict[date] = float(ninety_day[day]['SMA'])
-
-	thirtyday_x, thirtyday_y = zip(*sorted(thirty_dict.items()))
-	thirty_day_sma, = plt.plot(thirtyday_x, thirtyday_y, 'r', label="Thirty Day Simple Moving Average")
-
-	ninetyday_x, ninetyday_y = zip(*sorted(ninety_dict.items()))
-	ninety_day_sma, = plt.plot(ninetyday_x, ninetyday_y, 'g', label="Ninety Day Simple Moving Average")
-
-	plt.legend(handles=[thirty_day_sma, ninety_day_sma])
-
-	# All the days in the ninety day moving average are in the 30 day one
-
-	# -----
-	#ts = TimeSeries(key="Y9LU3HXNM65GKI9R")
-	price, price_md = ts.get_daily(symbol='.DJI', outputsize='full')
-	# price_x, price_y = zip(*sorted(price.items()))
-	price_dict = {}
-	for day in price:
-		date = datetime.datetime.strptime(day, "%Y-%m-%d").date()
-		price_dict[date] = float(price[day]['4. close'])
-	
-	price_x, price_y = zip(*sorted(price_dict.items()))
-	price_sma, = plt.plot(price_x, price_y)
-
-	plt.show()
+	print("Finished with " + str(capital) + " dollars.")
 
 if __name__ == "__main__":
-	API_KEY = sys.argv[1]
 
-	global ti
-	ti = TechIndicators(key=API_KEY)
-	global ts
-	ts = TimeSeries(key=API_KEY)
-	
-	simple_sma_trade(API_KEY, 1000, ".DJI")
-	#plot_averages()
+	simple_sma_trade(1000, 30, 90, "AAPL", datetime(2014, 1, 25), datetime(2018, 8, 1))
+	buy_and_hold(1000, "AAPL", datetime(2014, 1, 25), datetime(2018, 8, 1))
 
 
