894cfb32a98038da1df7994c63ad60ebc995fdc5
cooper-mj <coopermj@stanford.edu>
2018-08-13 16:52:52 -0700

    Added price and SMA graphs

---
+++ market_graphs.py
@@ -0,0 +1,42 @@
+from iexfinance import get_historical_data
+from datetime import datetime
+import matplotlib.pyplot as plt
+from simple_sma import N_day_sma
+
+'''
+Imports historical stock pricing data for the ticker stock within the range [start_date, 
+end_date], then uses matplotlib.pyplot to graph the price of the stock with respect 
+to the date. This function can graph daily prices from the set {open, high, low, close}.
+'''
+def price_graph(start_date, end_date, ticker, price_type="close"):
+
+	# Import stock data in the desired date range
+	df = get_historical_data(ticker, start=start_date, end=end_date, output_format='pandas')
+	df.index.name = 'date'
+	df.reset_index(inplace=True)
+
+	plt.plot(df['date'], df[price_type])	
+
+'''
+Calculates the N-day SMA for the ticker stock within the range [start_date, 
+end_date], then uses matplotlib.pyplot to graph the N-day SMA of the stock with 
+respect to the date. This function can calculate the SMA using daily prices 
+from the set {open, high, low, close}.
+'''
+def sma_graph(N, start_date, end_date, ticker, price_type="close"):
+
+	df = get_historical_data(ticker, start=start_date, end=end_date, output_format='pandas')
+	df.index.name = 'date'
+	df.reset_index(inplace=True)
+
+	df['SMA'] = df['date'].apply(lambda x: N_day_sma(N, df, datetime.strptime(str(x), "%Y-%m-%d"), start_date, price_type))
+
+	plt.plot(df['date'], df['SMA'])
+
+
+if __name__ == "__main__":
+	#Sample use
+	price_graph(datetime(2015, 1, 25), datetime(2018, 8, 1), "GS")
+	sma_graph(20, datetime(2015, 1, 25), datetime(2018, 8, 1), "GS")
+
+	plt.show()
