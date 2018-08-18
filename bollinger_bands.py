d36a67c4e9b7939b8e6cc4f35d2ea8afe860428d
cooper-mj <coopermj@stanford.edu>
2018-08-17 23:01:37 -0700

    Decomposed buy/sell

--- bollinger_bands.py
+++ bollinger_bands.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 from termcolor import colored, cprint
 
 from simple_sma import N_day_sma
+from buy_sell import sell
+from buy_sell import buy
 
 '''
 Calculates the N-day standard deviation of price. Uses price_type to
@@ -74,43 +76,23 @@ def bollinger_bands(starting_capital, period, stddev_coefficient, ticker, start_
 		lower_band = N_day_sma(period, df, date, start_date, price_type) - (stddev_coefficient * N_day_price_stddev(period, df, date, start_date, price_type))
 
 		if price > upper_band:
-			# If RSI > selling bound - sell signal
+			# If price is above the upper Bollinger Band - sell signal
 			if not liquid:
-				liquid = True
-				capital = num_shares * price
-				sell_msg = colored("Selling " + str(round(num_shares, 2)) + " shares of " + ticker + " at " + str(round(price, 2)) + " for " + str(round(capital, 2)), 'red')
-				print(date.strftime("%Y-%m-%d") + " : " + sell_msg)
-				num_shares = 0
+				sell_data = sell(liquid, date, ticker, capital, num_shares, price, previous_price, correct)
+				liquid = sell_data[0]
+				capital = sell_data[1]
+				num_shares = sell_data[2]
 
-				# Check to see if our last decision was the right one
-				if price > previous_price:
-					# Check if current price is less than price we bought for since
-					# our last call was a buy - check if we made money
-					correct[0] += 1
-					correct[1] += 1
-				else:
-					correct[1] += 1
-				previous_price = price
 
 		elif price < lower_band:
-			# If RSI < buying bound - buy signal
+			# If price is below the lower Bollinger Band - buy signal
 			if liquid:
-				liquid = False
-				num_shares = float(capital) / price
-				buy_msg = colored("Buying " + str(round(num_shares, 2)) + " shares of " + ticker + " at " + str(round(price, 2)) + " for " + str(round(capital, 2)), 'green')
-				print(date.strftime("%Y-%m-%d") + " : " + buy_msg)
-				capital = 0
+				buy_data = buy(liquid, date, ticker, capital, num_shares, price, previous_price, correct)
+				liquid = buy_data[0]
+				capital = buy_data[1]
+				num_shares = buy_data[2]
 
-				# Check to see if our last decision was the right one
-				if price < previous_price:
-					# Check if current price is less than price we bought for since
-					# our last call was a buy - check if we made money
-					correct[0] += 1
-					correct[1] += 1
-				else:
-					correct[1] += 1
-
-				previous_price = price
+		previous_price = price
 
 	if capital == 0:
 		# Sell the remaining shares usig the final date's price
