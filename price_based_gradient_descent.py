b9b6f7cc9f96d2f23c9e94216f11b6b3078e0a75
cooper-mj <coopermj@stanford.edu>
2018-10-02 23:59:52 -0700

    Added adjustible interval for dataset.

--- price_based_gradient_descent.py
+++ price_based_gradient_descent.py
@@ -22,7 +22,9 @@ ticker = input("Enter ticker: ")
 start_date = datetime(2015, 1, 1)
 end_date = datetime(2018, 10, 1)
 price_type = 'close'
+interval = int(input("Enter an interval for pricing within the dataset: "))
 
+# Generate dataset to train on
 df = get_historical_data(ticker, start=start_date, end=end_date, output_format='pandas')
 df.index.name = 'date'
 df.reset_index(inplace=True)
@@ -33,13 +35,13 @@ print(price_vals)
 
 dataset = []
 
-for i in range(330, len(price_vals[330:])):
-	x = price_vals[i-330: i-30: 30]
+for i in range(11 * interval, len(price_vals[11 * interval:])):
+	x = price_vals[i-(11*interval): i-interval: interval]
 	y = price_vals[i]
 	print((x, y))
 	dataset.append((x, y))
 		
-# Build dataset: x = 10-vector of prices from the previous 10 days; y = price on current day.
+# Build dataset: x = 10-vector of prices from the previous 10 prices, spaced 30-days apart; y = price on current day.
 print(df)
 print(dataset)
 price_based_gradient_descent(F, dF, 10)
