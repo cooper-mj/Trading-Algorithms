Added Adadelta step size adjustment to fix overflow.

price_based_gradient_descent.py
@@ -11,15 +11,15 @@ def dF(w):
 
 def price_based_gradient_descent(F, dF, d):
 	w = np.zeros(d)
-	eta = 0.0001 # Step size
-	for t in range(500): # Take 500 steps
+	eta = 0.001 # Step size
+	for t in range(1, 500): # Take 500 steps
 		value = F(w)
 		gradient = dF(w)
-		w = w - eta * gradient # Take one step
+		w = w - (eta/t) * gradient # Take one step
 		print("Iteration {}, w = {}, F(w) = {}, F\'(w) = {}".format(t, w, value, gradient))
 
 ticker = input("Enter ticker: ")
-start_date = datetime(2018, 1, 1)
+start_date = datetime(2015, 1, 1)
 end_date = datetime(2018, 10, 1)
 price_type = 'close'
 
@@ -33,12 +33,13 @@ print(price_vals)
 
 dataset = []
 
-for i in range(11, len(price_vals[11:])):
-	x = price_vals[i-11: i-1]
+for i in range(330, len(price_vals[330:])):
+	x = price_vals[i-330: i-30: 30]
 	y = price_vals[i]
 	print((x, y))
 	dataset.append((x, y))
 		
 # Build dataset: x = 10-vector of prices from the previous 10 days; y = price on current day.
 print(df)
+print(dataset)
 price_based_gradient_descent(F, dF, 10)